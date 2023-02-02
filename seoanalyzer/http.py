db1cc4fd18c20b139acbe4454155df9e548eb72e
jaiantony <63301313+Jaiantony@users.noreply.github.com>
2023-02-02 16:06:01 +0530

    Update http.py

--- seoanalyzer/http.py
+++ seoanalyzer/http.py
@@ -1,13 +1,18 @@
 import certifi
 from urllib3 import PoolManager
 from urllib3 import Timeout
+import random
+from fake_useragent import UserAgent
 
 
 class Http():
     def __init__(self):
-        user_agent = {'User-Agent': 'Mozilla/5.0'}
+        user_agent = UserAgent()
+        user_agent = user_agent.random
+        # user_agent = ["Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)","Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/W.X.Y.Z Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)","Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36 (compatible; Google-Read-Aloud; +https://developers.google.com/search/docs/advanced/crawling/overview-google-crawlers)","Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36 (compatible; Google-Read-Aloud; +https://developers.google.com/search/docs/advanced/crawling/overview-google-crawlers)"]
+        user_agent = {"User-Agent": user_agent}
         self.http = PoolManager(
-            timeout=Timeout(connect=1.0, read=2.0),
+            # timeout=Timeout(connect=1.0, read=2.0),
             cert_reqs='CERT_REQUIRED',
             ca_certs=certifi.where(),
             headers=user_agent
@@ -16,5 +21,5 @@ class Http():
     def get(self, url):
         return self.http.request('GET', url)
 
+
 http = Http()
-        
